ff6e9ffe9f0529ab75f13d89238dc30e285208f9
evwalles <49479356+evwalles@users.noreply.github.com>
2020-02-19 17:57:19 -0800

    added comments

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,22 @@
+#Specifies the base image of linux that the app will run on.
 FROM python:alpine3.10
 
+#Copies the files in the current directory to the '/app' directory.
+#The files and directories in the '.dockerignore' file will not be
+#copied
 COPY . /app
 
+#Sets the working directory inside the container
 WORKDIR /app
 
+#Runs updates
 RUN apk add --update python py-pip
 
+#Installs components specified in the 'requirements.txt' file
 RUN pip install -r requirements.txt
 
+#Sets the port that the docker container will listen on
 EXPOSE 8000
 
+#Runs the app.py application
 CMD ["python", "/app/app.py", "-p 8000"]
--- app.py
+++ app.py
@@ -5,10 +5,16 @@ from pymongo import MongoClient
 import sys
 import optparse
 
+#Initialize Flask and MongoDB
 app = Flask(__name__)
 client = MongoClient('db', 27017)
 db = client.peopledb
 
+#Home page of the web app.
+#At first, it displays all people that are currently
+#stored in the database. If the user chooses to add
+#a name, then it is added to the database, and the page
+#is reloaded with the updated database content.
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -25,6 +31,10 @@ def index():
         people = [person for person in _people]
         return render_template('index.html', people=people)
 
+#If the user chooses to delete a name from the datbase,
+#the name gets passed in, and the database is queried for
+#that name. All people in the database with a matching name
+#will be removed.
 @app.route('/delete/<string:name>')
 def delete(name):
 
@@ -34,6 +44,11 @@ def delete(name):
     except:
         return 'There was a problem deleting that person'
 
+#If the user chooses to update a name in the database, they are
+#taken to 'update.html' which allows them to enter a new name.
+#Then the both names are passed to the this function. The old name
+#is used to query the database, and then all matches will be updated
+#to the new name.
 @app.route('/update/<string:name>', methods=['GET', 'POST'])
 def update(name):
 
